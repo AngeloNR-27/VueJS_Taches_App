714967ab240348fe7fefce214fe74bb263fca260
AngeloNR-27 <rabetsimbaangelo@gmail.com>
2024-02-26 12:05:02 +0300

    Deconnexion creation methodes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Tasks from '../components/Tasks.vue' // Assurez-vous que le chemin d'importation est correct
+import Tasks from '../components/Tasks.vue' 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,9 +16,9 @@ const router = createRouter({
       component: () => import('../views/HomeView.vue')
     },
     {
-      path: '/tasks', // Assurez-vous que le chemin est correct
-      name: 'tasks', // Ce nom doit correspondre à celui utilisé dans this.$router.push
-      component: () => import('../components/Tasks.vue') // Assurez-vous que le chemin d'importation est correct
+      path: '/tasks', 
+      name: 'tasks', 
+      component: () => import('../components/Tasks.vue') 
     }    
   ]
 })
